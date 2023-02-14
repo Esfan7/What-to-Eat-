317794af53224529c0b85f26f1427cb15c67de40
Esfan7 <115903924+Esfan7@users.noreply.github.com>
2023-02-14 11:28:12 -0800

    Update script.js

--- 404_page/script.js
+++ 404_page/script.js
@@ -1,19 +1,4 @@
 function redirectFunction(){
-    window.location.href = "../index.html";
+    window.location.href = "index.html";
 }
 document.getElementById("return").addEventListener("click", redirectFunction);
-
-var badRequestUrl = g;
-var redirectUrl = './404_Page/404.html';
-
-fetch(badRequestUrl).then(function (response) {
-    // Check the response value is equal to 404.
-    if (response.status === 404) {
-      // If the page is not on the 404 page, redirect to it.
-      document.location.replace(redirectUrl);
-    } else {
-      return response.json();
-    }
-  });
-
-
