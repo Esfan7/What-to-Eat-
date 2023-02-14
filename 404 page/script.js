b9bcee833c5dfa9fc34f5639e6f0237fd4d0627c
Esfan B <eafanbeh@gmail.com>
2023-02-13 21:59:07 -0800

    cleaned up html

--- 404 page/script.js
+++ 404 page/script.js
@@ -1,4 +1,19 @@
 function redirectFunction(){
-    window.location.href = "restaurant.html";
+    window.location.href = "../index.html";
 }
-document.getElementById("mainBtn").addEventListener("click", redirectFunction);
+document.getElementById("return").addEventListener("click", redirectFunction);
+
+var badRequestUrl = g;
+var redirectUrl = './404 Page/404.html';
+
+fetch(badRequestUrl).then(function (response) {
+    // Check the response value is equal to 404.
+    if (response.status === 404) {
+      // If the page is not on the 404 page, redirect to it.
+      document.location.replace(redirectUrl);
+    } else {
+      return response.json();
+    }
+  });
+
+
